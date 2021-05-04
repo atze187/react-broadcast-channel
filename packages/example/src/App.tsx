6246f512e2bd884813024b74ee59420dbd083dd4
jhony-24 <jhonyvegacuya24@gmail.com>
2021-05-04 07:40:27 -0500

    docs: update example package from codesanbox

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,21 +1,52 @@
-import React from 'react';
+import React,{ useEffect, useRef, useState } from "react";
 import { useBroadcastChannel  } from "./lib"
-
 
 const TEST_CHANNEL = "TEST_CHANNEL";
 
+function SubmitMessage() {
+  const { emit } = useBroadcastChannel(TEST_CHANNEL);
+  const input = useRef<HTMLInputElement>(null);
+  
+  const sendMessage = () => {
+    emit({
+      message: input.current.value
+    });
+  };
+
+  return (
+    <header>
+      <input ref={input} />
+      <button onClick={sendMessage}>send a message</button>
+    </header>
+  );
+}
+
+const ListMessages = () => {
+  const [messages, setMessages] = useState([]);
+  const { subscribe } = useBroadcastChannel(TEST_CHANNEL);
+  useEffect(() => {
+    const unsubscribe = subscribe((message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  return (
+    <ol>
+      {messages.map((item, id) => (
+        <li key={id}>{item.message}</li>
+      ))}
+    </ol>
+  );
+};
+
 
 export default function App() {
-  const { emit, data } = useBroadcastChannel(TEST_CHANNEL);
-
-  const sendMessage = () => {
-    emit(prompt())
-  }
-
-  return(
-    <div>
-      <button onClick={sendMessage}>emit event</button>
-      <h1>{data}</h1>
+  return (
+    <div className="App">
+      <SubmitMessage />
+      <ListMessages />
     </div>
-  )
+  );
 }
